3f1d94bd959399f6837d612c9acc1ddaf2a1f2b0
Amanda Silva <amandasilva@Amandas-MacBook-Pro.local>
2018-03-22 20:30:09 -0500

    updated file

--- 01Week/DataTypesAndExpressions.cs
+++ 01Week/DataTypesAndExpressions.cs
@@ -5,7 +5,35 @@ public class Program
 {
     public static void Main()
     {
-        // your code goes here
+        bool isHuman = true;
+
+        bool f = false;
+
+        decimal num = 9.99m;
+
+        decimal total = num * num;
+
+        string firstName = "Roberto";
+        string lastName = "Navarro";
+        int age = 35;
+        string job = "Developer";
+        string favoriteBand = "AC/DC";
+        string favoriteSportsTeams = "Spurs";
+
+        Console.WriteLine("First Name: " + firstName);
+        Console.WriteLine("Last Name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Job: " + job);
+        Console.WriteLine("Favorite Band: " + favoriteBand);
+        Console.WriteLine("Favorite Sports Team: " + favoriteSportsTeams);
+
+
+        int myInteger = (int)num;
+
+        Console.WriteLine("My Integer: " + myInteger);
+
+
+
 
 
         // leave this command at the end so your program does not close automatically
