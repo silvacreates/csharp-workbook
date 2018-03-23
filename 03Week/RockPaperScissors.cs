6e0e4916950ff81c44d669d7a7521951f10b9b9c
Amanda Silva <amandasilva@Amandas-MacBook-Pro.local>
2018-03-22 20:39:56 -0500

    udpated RockPaperScissors

--- 03Week/RockPaperScissors.cs
+++ 03Week/RockPaperScissors.cs
@@ -4,16 +4,45 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Enter hand 1:");
+        Console.WriteLine(">>>>Rock, Paper, Scissors<<<<<");
+
+        Console.Write("Enter hand 1 (rock, paper or scissors): ");
         string hand1 = Console.ReadLine().ToLower();
-        Console.WriteLine("Enter hand 2:");
+
+        Console.Clear();
+        Console.WriteLine(">>>>Rock, Paper, Scissors<<<<<");
+        Console.Write("Enter hand 2 (rock, paper or scissors): ");
         string hand2 = Console.ReadLine().ToLower();
+
         Console.WriteLine(CompareHands(hand1, hand2));
+
+        Console.ReadLine();
     }
-    
+
     public static string CompareHands(string hand1, string hand2)
     {
-        // Your code here
-        return hand1 + " " + hand2;
+        string message = "";
+        bool tie = false;
+        tie = (hand1 == hand2) ? true : false;
+
+        if (tie) message = "It's a tie!";
+
+        switch (hand1)
+        {
+            case "rock":
+                if (hand2 == "scissors") message = "Rock beats scissors, Hand one wins!";
+                else message = "Paper beats rock, Hand two wins!";
+                break;
+            case "paper":
+                if (hand2 == "rock") message = "Paper beats rock, Hand one wins!";
+                else message = "Scissors beats paper, Hand two wins!";
+                break;
+            case "scissors":
+                if (hand2 == "paper") message = "Scissors beats paper, Hand one wins!";
+                else message = "Rock beats scissors, Hand two wins!";
+                break;
+        }
+
+        return message;
     }
 }
